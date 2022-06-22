46134f1f979c1b21f28c17db510c32db4cb21be6
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2022-06-22 11:16:25 +0300

    Exercise 1.07

--- logger/index.js
+++ logger/index.js
@@ -1,8 +1,22 @@
+require('dotenv').config()
 const uuid = require('uuid')
+const express = require('express')
 
+const app = express()
+const port = process.env.PORT || 3001
 const s = uuid.v4()
 
+let time = new Date()
+
 let interval = setInterval(() => {
-  const now = new Date()
-  console.log(`${now.toISOString()}: ${s}`)
+  time = new Date()
+  console.log(`${time.toISOString()}: ${s}`)
 }, 5 * 1000)
+
+app.get('/', (req, res) => {
+  res.send(`${time.toISOString()}: ${s}`)
+})
+
+app.listen(port, () => {
+  console.log(`Logger running on port ${port}`)
+})
