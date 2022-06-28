f0dd97ccbb7ad33ca3a9eb5467c1dc87865288b5
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2022-06-28 17:06:53 +0300

    Exercise 2.10

--- todo-server/src/index.js
+++ todo-server/src/index.js
@@ -11,6 +11,11 @@ app.use(express.json())
 app.use(cors())
 
 app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} ${JSON.stringify(req.body)}`)
+  next()
+})
+
+app.use((req, res, next) => {
   const prisma = new PrismaClient()
   req.prisma = prisma
   next()
--- todo-server/src/routes/todo.js
+++ todo-server/src/routes/todo.js
@@ -10,15 +10,23 @@ todoRouter.get('/', async (req, res) => {
 })
 
 todoRouter.post('/', async (req, res) => {
-  try {
-    const todo = await req.prisma.todo.create({
-      data: {
-        text: req.body.text,
-      }
-    })
-    res.json(todo)
-  } catch (err) {
-    res.status(400).send(err.message)
+  if (!req.body.text) {
+    console.log("Missing text")
+    res.status(400).json('Text is required')
+  } else if (req.body.text.length > 140) {
+    console.log(`Text is too long (${req.body.text.length} chars): "${req.body.text}"`)
+    res.status(400).json('Text is too long')
+  } else {
+    try {
+      const todo = await req.prisma.todo.create({
+        data: {
+          text: req.body.text,
+        }
+      })
+      res.json(todo)
+    } catch (err) {
+      res.status(400).send(err.message)
+    }
   }
 })
 
