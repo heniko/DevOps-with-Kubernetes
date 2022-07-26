0f26b7f3a74339493c6a59c9be472cb3b75cd305
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2022-07-27 00:30:21 +0300

    Fix ingress, fix app warning

--- todo-server/src/index.js
+++ todo-server/src/index.js
@@ -6,6 +6,7 @@ const { PrismaClient } = require('@prisma/client')
 const app = express()
 
 const port = process.env.PORT || 3001
+const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(cors())
@@ -16,7 +17,6 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-  const prisma = new PrismaClient()
   req.prisma = prisma
   next()
 })
