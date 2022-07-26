73240d8b5896b73f01efa59d6abff9edbc4da4db
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2022-07-27 02:24:49 +0300

    Test web deployment

--- todo/src/services/todo.ts
+++ todo/src/services/todo.ts
@@ -6,7 +6,7 @@ const baseUrl = `${apiUri()}/api/todos`
 
 export const getAll = async () => {
   try {
-    const { data } = await axios.get<Array<Todo>>(baseUrl)
+    const { data } = await axios.get<Array<Todo>>('/api/todos')
     return data as Array<Todo>
   } catch (error) {
     console.error(error)
@@ -16,7 +16,7 @@ export const getAll = async () => {
 
 export const create = async (todo: String) => {
   try {
-    const { data } = await axios.post<Todo>(baseUrl, { text: todo })
+    const { data } = await axios.post<Todo>('/api/todos', { text: todo })
     return data as Todo
   } catch (error) {
     console.error(error)
